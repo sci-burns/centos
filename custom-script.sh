9eff9776b404ba73610cc1932075fadfc14a9c4b
DavidBurns <david.burns@pega.com>
2016-10-04 09:14:27 -0400

    ISSUE-0 experimental customizations

--- custom-script.sh
+++ custom-script.sh
@@ -7,3 +7,28 @@ set -eux
 #
 # For example:
 # yum install -y curl wget git tmux firefox xvfb
+# Install docker
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+sudo yum install -y docker-engine-1.11.2
+
+# Enable docker to take commands over http
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo tee /etc/systemd/system/docker.service.d/docker.conf <<-'EOF'
+[Service]
+ExecStart=
+ExecStart=/usr/bin/docker daemon -H tcp://0.0.0.0:2375 -H fd://
+EOF
+
+# Docker starts at boot.
+sudo chkconfig docker on
+
+# Add the docker admin to the docker group
+usermod -a -G docker dockeradmin
--- script/cleanup.sh
+++ script/cleanup.sh
@@ -105,6 +105,12 @@ echo '==> Zeroing out empty area to save space in the final image'
 dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed"
 rm -f /EMPTY
 
+echo '==> Zeroing out empty area to save space in the final image'
+# Zero out the free space to save space in the final image.  Contiguous
+# zeroed space compresses down to nothing.
+dd if=/dev/zero of=/var/lib/EMPTY bs=1M || echo "dd exit code $? is suppressed"
+rm -f /var/lib/EMPTY
+
 # Block until the empty file has been removed, otherwise, Packer
 # will try to kill the box while the disk is still full and that's bad
 sync
@@ -114,3 +120,6 @@ echo ${DISK_USAGE_BEFORE_CLEANUP}
 
 echo "==> Disk usage after cleanup"
 df -h
+
+echo "==> Remove vagrant from sodoers"
+rm /etc/sudoers.d/vagrant
