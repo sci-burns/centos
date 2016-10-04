996422fe6486cd928df69ddd347b1b584cc7538d
DavidBurns <david.burns@pega.com>
2016-10-04 17:38:41 -0400

    ISSUE-0 try to include ovf wrapping

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -108,7 +108,7 @@ rm -f /EMPTY
 echo '==> Zeroing out empty area to save space in the final image'
 # Zero out the free space to save space in the final image.  Contiguous
 # zeroed space compresses down to nothing.
-dd if=/dev/zero of=/var/lib/EMPTY bs=1M || echo "dd exit code $? is suppressed"
+dd if=/dev/zero of=/var/lib/EMPTY bs=1M count=10000 || echo "dd exit code $? is suppressed"
 rm -f /var/lib/EMPTY
 
 # Block until the empty file has been removed, otherwise, Packer
