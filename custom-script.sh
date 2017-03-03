f9bbc0db4b81a74d6a00bd4d5b30d0bce49e58d8
DavidBurns <david.burns@pega.com>
2017-03-02 20:48:14 -0500

    US-160899 some template touch ups

--- custom-script.sh
+++ custom-script.sh
@@ -24,7 +24,7 @@ sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo tee /etc/systemd/system/docker.service.d/docker.conf <<-'EOF'
 [Service]
 ExecStart=
-ExecStart=/usr/bin/docker daemon -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock
+ExecStart=/usr/bin/docker daemon -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --storage-opt dm.basesize=15G
 EOF
 
 # Docker starts at boot.
@@ -36,17 +36,21 @@ usermod -a -G docker dockeradmin
 # Docker-Compose
 sudo curl -o /usr/local/bin/docker-compose -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m`
 sudo chmod +x /usr/local/bin/docker-compose
+echo -e "\nexport PATH=$PATH:/usr/local/bin" | sudo tee -a /root/.bash_profile
 
 sudo yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel net-tools wget
+
+# Add timestamps to bash history
+echo 'export HISTTIMEFORMAT="%y/%m/%d %T "' | sudo tee -a /root/.bash_profile
 
 # Install latest git
 cd ~
 sudo yum install -y epel-release
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo rpm -Uvh epel-release-latest-7*.rpm
+sudo rpm -Uvh epel-release-latest-7*.rpm | true
 
 wget https://centos7.iuscommunity.org/ius-release.rpm
-sudo rpm -Uvh ius-release*.rpm
+sudo rpm -Uvh ius-release*.rpm | true
 
 sudo yum install -y git2u
 
