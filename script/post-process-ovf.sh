085b27bbcb87e877482600424cc4585a906f67d6
root <root@senglnx55.rpega.com>
2017-04-11 15:08:56 -0400

    TASK-0 zip issue

--- script/post-process-ovf.sh
+++ script/post-process-ovf.sh
@@ -7,5 +7,5 @@ export VM_VERSION=1.3
 if [ -f "output-centos72-vmware-iso/centos72.vmx" ]; then
 	ovftool output-$VM_NAME-vmware-iso/$VM_NAME.vmx box/ovf/$VM_NAME-$VM_VERSION.ovf
 	cd box/ovf
-	zip -r $VM_NAME-$VM_VERSION.zip $VM_NAME
+	zip -r $VM_NAME-$VM_VERSION.zip $VM_NAME*
 fi
